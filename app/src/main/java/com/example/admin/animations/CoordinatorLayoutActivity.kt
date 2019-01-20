451a9a088a492313557cb39bcc78023f2a1213f7
Sanket <vetkolisanket@gmail.com>
2019-01-20 16:42:15 +0530

    Added a basic FabActivity

--- app/src/main/java/com/example/admin/animations/CoordinatorLayoutActivity.kt
+++ app/src/main/java/com/example/admin/animations/CoordinatorLayoutActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_coordinator_layout.*
 
 class CoordinatorLayoutActivity : AppCompatActivity() {
 
@@ -14,5 +15,11 @@ class CoordinatorLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coordinator_layout)
+
+        init()
+    }
+
+    private fun init() {
+        btnFab.setOnClickListener { startActivity(FabActivity.newIntent(this)) }
     }
 }
---
+++ app/src/main/java/com/example/admin/animations/FabActivity.kt
@@ -0,0 +1,28 @@
+package com.example.admin.animations
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_fab.*
+
+class FabActivity : AppCompatActivity() {
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, FabActivity::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fab)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
