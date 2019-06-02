7549eefc3b28c7c4c80e7c40941ff0f201f8f5fe
Sanket <vetkolisanket@gmail.com>
2019-06-02 19:05:19 +0530

    Added two views for multi fab which are toggled on fab clicked

--- app/src/main/java/com/example/admin/animations/constraint/MultiFabActivity.kt
+++ app/src/main/java/com/example/admin/animations/constraint/MultiFabActivity.kt
@@ -2,11 +2,22 @@ package com.example.admin.animations.constraint
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
+import android.transition.ChangeBounds
+import android.transition.TransitionManager
+import android.view.View
+import android.view.animation.DecelerateInterpolator
 import com.example.admin.animations.R
+import kotlinx.android.synthetic.main.activity_multi_fab.*
 
 class MultiFabActivity : AppCompatActivity() {
+
+    private val csOld by lazy {   ConstraintSet() }
+    private val csNew by lazy {  ConstraintSet() }
+    private var activated = false
 
     companion object {
         fun newIntent(context: Context) = Intent(context, MultiFabActivity::class.java)
@@ -15,5 +26,27 @@ class MultiFabActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_multi_fab)
+
+        init()
     }
+
+    private fun init() {
+        csOld.clone(parentLayout)
+        csNew.clone(this, R.layout.activity_multi_fab_alt)
+    }
+
+    fun swapLayout(view: View) {
+        activated = !activated
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val changeBounds = ChangeBounds()
+            changeBounds.interpolator = DecelerateInterpolator()
+            TransitionManager.beginDelayedTransition(parentLayout, changeBounds)
+        }
+        if (activated) {
+            csNew.applyTo(parentLayout)
+        } else {
+            csOld.applyTo(parentLayout)
+        }
+    }
+
 }
